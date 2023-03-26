36a915f85f633db7afade9f71c6b8c75130c772e
GasparCordero <cordero647@gmail.com>
2023-03-26 16:56:26 -0300

    Modifique app.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express();
 const publicPath = path.resolve(__dirname, "./public")
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log("Servidor 3000 corriendo correctamente")
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Servidor corriendo correctamente en el puerto " + port)
+);
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
